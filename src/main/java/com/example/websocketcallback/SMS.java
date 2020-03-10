af61b0d0fbfe73e7f4c6f8e681d6edd9ccaa96c9
Marc Schlossberg <mr.schlossberg@gmail.com>
2020-03-09 21:45:20 -0400

    WIP/Following Twilio tutorial

---
+++ src/main/java/com/example/websocketcallback/SMS.java
@@ -0,0 +1,30 @@
+package com.example.websocketcallback;
+
+public class SMS {
+    private String to;
+    private String message;
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "SMS{" +
+                "to='" + to + '\'' +
+                ", message='" + message + '\'' +
+                '}';
+    }
+}
